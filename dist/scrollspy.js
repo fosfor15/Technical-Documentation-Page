8f56613814b3e2aab2a8eb11022c3e7809ccb498
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-02-12 21:55:44 +0300

    Build button-to-up

--- dist/scrollspy.js
+++ dist/scrollspy.js
@@ -1,7 +1,8 @@
 
 const navLinks = document.getElementsByClassName('nav-link');
-const sectionsHeaders = document.querySelectorAll('.main-section header a');
 const sections = document.getElementsByClassName('main-section');
+const sectionsHeaders = document.querySelectorAll('.main-section header a');
+const buttonToUp = document.getElementById('button-to-up');
 
 let currentScrollPos = 0;
 let currentSection = 0;
@@ -9,18 +10,17 @@ let currentSection = 0;
 window.addEventListener('scroll', function() {
     currentScrollPos = window.pageYOffset;
 
-    if (sections[currentSection + 1]?.offsetTop <= currentScrollPos) {
+    if (sections[currentSection + 1]?.offsetTop - 2 <= currentScrollPos) {
         processScrollHighlight();
         currentSection += 1;
         processScrollHighlight();
 
-    } else if (sections[currentSection]?.offsetTop > currentScrollPos) {
+    } else if (sections[currentSection]?.offsetTop - 2 > currentScrollPos) {
         processScrollHighlight();
         currentSection -= 1;
         processScrollHighlight();
 
-    } else if (document.documentElement.scrollHeight - document.documentElement.clientHeight
-         == currentScrollPos) {
+    } else if (document.documentElement.scrollHeight - document.documentElement.clientHeight == currentScrollPos) {
         processScrollHighlight();
         currentSection += 1;
         processScrollHighlight();
@@ -30,4 +30,14 @@ window.addEventListener('scroll', function() {
         navLinks[currentSection].classList.toggle('active');
         sectionsHeaders[currentSection].classList.toggle('active');
     }
+
+    if (document.documentElement.clientHeight <= currentScrollPos) {
+        buttonToUp.style.display = 'flex';
+    } else {
+        buttonToUp.style.display = '';
+    }
+});
+
+buttonToUp.addEventListener('click', function() {
+    window.scrollTo(0, 0);
 });
